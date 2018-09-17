dae0b80b2446aceb43ce95f364bac2f49dac8e2e
jharris-code <30908097+jharris-code@users.noreply.github.com>
2018-09-17 16:06:11 -0700

    use .env variables

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -6,25 +6,12 @@ import {
   LOGOUT
 } from '../actions/auth'
 
-// const initialState = {
-//   auth0: new auth0.WebAuth({
-//     domain: `${process.env.AUTH_CONFIG_DOMAIN}`,
-//     clientID: `${process.env.AUTH_CONFIG_CLIENTID}`,
-//     redirectUri: `${process.env.AUTH_CONFIG_CALLBACK}`,
-//     responseType: 'token id_token',
-//     scope: 'openid'
-//   }),
-//   access_token: null,
-//   id_token: null,
-//   expires_at: null
-// }
-
-
+// TODO: do not include .env files in git, split out var's by environment
 const initialState = {
   auth0: new auth0.WebAuth({
-    domain: 'semada.auth0.com',
-    clientID: 'gUl64pYcPXvZy02L-U-KPL5CAPvHO3YD',
-    redirectUri: 'http://localhost:3000/callback',
+    domain: process.env.REACT_APP_AUTH_CONFIG_DOMAIN,
+    clientID: process.env.REACT_APP_AUTH_CONFIG_CLIENTID,
+    redirectUri: process.env.REACT_APP_AUTH_CONFIG_CALLBACK,
     responseType: 'token id_token',
     scope: 'openid'
   }),
@@ -37,6 +24,7 @@ const initialState = {
 const auth = (state = initialState, action) => {
   switch(action.type) {
   case LOGIN:
+    console.log(process.env.REACT_APP_AUTH_CONFIG_DOMAIN)
     let tempAuth0 = merge({}, state.auth0, action.auth0)
     return merge({}, state, {auth0: tempAuth0, auth : true})
   case AUTHENTICATE:
